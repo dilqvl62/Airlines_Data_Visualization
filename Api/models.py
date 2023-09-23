a28e71d1b20eb14ab1295cf6821fb24a56ea359e
dilqvl62 <sab38m@hotmail.com>
2023-09-23 18:30:09 -0500

    fixed a spelling error

--- Api/models.py
+++ Api/models.py
@@ -1,4 +1,4 @@
 from app import Base
 total_flights = Base.classes.total_flights
 total_time_delayed = Base.classes.ttl_delayed_duration
-canceled_flights_total = Base.classes.canceled_flights_total
+cancelled_flights_total = Base.classes.cancelled_flights_total
