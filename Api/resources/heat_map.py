e31e6d3fb1777635571263fb4098c0302034b2db
dilqvl62 <sab38m@hotmail.com>
2023-09-23 18:27:31 -0500

    calculated the percentage of cancelled flights

--- Api/resources/heat_map.py
+++ Api/resources/heat_map.py
@@ -1,12 +1,12 @@
 from flask import jsonify, make_response
 from flask_restful import Resource
-from models import canceled_flights_total
+from models import cancelled_flights_total
 import app
 
 class heatMap(Resource):
     def get(self):
         try:
-            rows = app.session.query(canceled_flights_total).all()
+            rows = app.session.query(cancelled_flights_total).all()
             results = []
             for total_cancelled in rows:
                 results.append( {
@@ -14,7 +14,9 @@ class heatMap(Resource):
                                     "airportCode":total_cancelled.Airport_Code,
                                     "Latitude":total_cancelled.Latitude, 
                                     "Longitude":total_cancelled.Longitude,
-                                    "total_flights_canceled":total_cancelled.Cancelled_flights
+                                    "total_arriving_flights":total_cancelled.total_arriving_flights,
+                                    "total_flights_cancelled":total_cancelled.Cancelled_flights,
+                                    "cancelled_percentage" : total_cancelled.Cancelled_percentage
                                  }
                                )
             return make_response(jsonify(results), 200)
